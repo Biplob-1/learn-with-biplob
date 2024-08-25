980eeefb8ef5b4b57d877809931d14a3880b5688
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-08-25 21:24:13 +0600

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-4 bg-blue-500">
+      <h1 className="text-white text-2xl">Hello, Tailwind and DaisyUI!</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
+
+export default App;
